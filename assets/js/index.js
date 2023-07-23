e70ee143dd1cf98294a2597b7734ea80912e38b5
Mariano García <marianogarciagz@gmail.com>
2023-07-22 18:26:34 -0600

    print tones in HTML

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,74 +1,67 @@
+const $frecuencyTableDOM = document.getElementById("f-table");
+const $tuningInput = document.getElementById("tuningInput");
+const $calculateButton = document.getElementById("calculateButton");
 
+const notesList = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
 
-/* const tuning = parseInt(prompt()); */
+function calculateFrequencies(tuning) {
+    const pianoArray = [...Array(10)].map(() => Array(12).fill(0));
 
+    for (let x = 0; x < pianoArray.length; x++) {
+        for (let y = 0; y < pianoArray[x].length; y++) {
+            const hz_distance = Math.pow(2, y / 12);
+            pianoArray[x][y] = (Math.pow(2, x) * (tuning / 8)) * hz_distance;
+            pianoArray[x][y] = pianoArray[x][y].toFixed(5);
+        }
+    }
 
-const tuning = 440
-
-
-
-if (tuning < 220 || tuning > 880) {
-    alert("Invalid Tuning Frequency");
-} else {
-    alert(`Tuning frequency is set at ${tuning}`);
+    return pianoArray;
 }
 
+$calculateButton.addEventListener("click", () => {
+    const tuning = parseFloat($tuningInput.value);
 
+    if (isNaN(tuning) || tuning < 220 || tuning > 880) {
+        alert("Invalid Tuning Frequency");
+    } else {
+        const pianoArray = calculateFrequencies(tuning);
+        populateTable(pianoArray);
+    }
+});
 
-let notesList = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
-let pianoArray = [...Array(10)].map(() => Array(12).fill(0));
+function populateTable(pianoArray) {
+    $frecuencyTableDOM.innerHTML = ""; // Clear existing content
 
-for (let x = 0; x < pianoArray.length; x++) {
-    for (let y = 0; y < pianoArray[x].length; y++) {
-        /* distance between notes = 12-tones equal temperament */
-        let hz_distance = Math.pow(2, y / 12);
-        // rounded to 2 decimals ✅
-        // pianoArray[x][y] = Math.round((Math.pow(2, x) * (tuning / 8)) * (hz_distance) * 100) / 100;
-        /*  */
-        pianoArray[x][y] = (Math.pow(2, x) * (tuning / 8)) * (hz_distance);
-        pianoArray[x][y] = pianoArray[x][y].toFixed(5)
-        switch (y) {
-            case 0:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 1:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 2:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 3:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 4:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 5:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 6:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 7:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 8:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 9:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 10:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 11:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            case 12:
-                console.log(`${x}${notesList[y]}: ${pianoArray[x][y]} hz`);
-                break;
-            default:
-                break;
+    for (let x = 0; x < pianoArray.length; x++) {
+        for (let y = 0; y < pianoArray[x].length; y++) {
+            const note = `${x}${notesList[y]}`;
+            const frequency = `${pianoArray[x][y]} hz`;
+
+            const tr = document.createElement("tr");
+            switch (y) {
+                case 1:
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    tr.classList.add("f-table__row--black");
+                    break;
+                default:
+                    tr.classList.add("f-table__row");
+                    break;
+            }
+
+            const noteCell = document.createElement("td");
+            noteCell.classList.add("f-table__note");
+            noteCell.textContent = note;
+            tr.appendChild(noteCell);
+
+            const frequencyCell = document.createElement("td");
+            frequencyCell.classList.add("f-table__frecuency");
+            frequencyCell.textContent = frequency;
+            tr.appendChild(frequencyCell);
+
+            $frecuencyTableDOM.appendChild(tr);
         }
     }
 }
